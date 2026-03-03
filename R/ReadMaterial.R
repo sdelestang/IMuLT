f150e501a3e7d3c3a2c60d9090c9eb1d8d095ac4
Simon de Lestang <simon.delestang@fish.wa.gov.au>
2026-03-04 06:33:33 +0800

    Revert "Klass fix to read me length weight"
    This reverts commit 1d44c868d4f4831bfc8774d7e64c2cd10320b6ca.

--- R/ReadMaterial.R
+++ R/ReadMaterial.R
@@ -661,7 +661,7 @@ ReadControlFile <- function(ControlFile,GeneralSpecs,DataSpecs)
   for (Isex in 1:GeneralSpecs$Nsex)
   {
     for (Jlen in 1:GeneralSpecs$Nlen[Isex]) WeightLen[Isex,Jlen] <- as.numeric(ControlFile[Index,Jlen])
-    if (!is.na(ControlFile[Index,GeneralSpecs$Nlen[Isex]+1]) & ControlFile[Index,GeneralSpecs$Nlen[Isex]+1]!="") { print("Error reading weight-at-length; too many inputs: Stopping"); AAA }
+    if (!is.na(ControlFile[Index,GeneralSpecs$Nlen[Isex]+1])) { print("Error reading weight-at-length; too many inputs: Stopping"); AAA }
     Index <- Index + 1
   }
   write("Weight-length regressions",EchoFile,append=T)

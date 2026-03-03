2f22a8d465477e1289e8430957fc2ecafa2fa8d1
jasusedwardsii <klaas.hartmann@gmail.com>
2026-03-04 09:04:14 +1100

    Revert "Merge branch 'main' of https://github.com/sdelestang/IMuLT"
    This reverts commit ef6c0f754abcae78e7a8e39a6b85c00f659cd1db, reversing
    changes made to 921a4aa55fbe42b9ed88782d354e2268adc2c37d.

--- R/imult_usage.R
+++ R/imult_usage.R
@@ -32,7 +32,9 @@ BASIC WORKFLOW:
    UpdateLFWeights('No')
 
 5. Set output options and run model:
+   FullOutput <- FALSE
    LoadData()
+   Data$DoProject <- 0
    LoadPars()
    SolveModelNew(500, 2000, report = TRUE)
    MakeDiagReport(is95 = FALSE)

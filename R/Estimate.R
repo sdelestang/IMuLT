d7c30226f224048c321a8e27e45df56ac875b03a
Simon de Lestang <simon.delestang@fish.wa.gov.au>
2026-04-23 14:32:23 +0800

    fixing the yy issue

--- R/Estimate.R
+++ R/Estimate.R
@@ -1074,7 +1074,7 @@ FitModel <- function(phit = 500, lphit = 1000, mxph = MaxPhase,
       yy <<- ifelse(is.na(tyy), yy, tyy)
       FnCallNo <<- FnCallNo + 1
 
-      if (BestFn > yy) {
+      if (!is.na(yy) && BestFn > yy) {
         BestFn <<- yy
         if ((FnCallNo %% PrintLag) == 0) {
           delta <- 100 * (1 - (yy / LastPrintFn))

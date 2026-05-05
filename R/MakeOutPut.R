740e6c2a76c8bf0cacff52374d0b0a61d52e1818
Simon de Lestang <simon.delestang@fish.wa.gov.au>
2026-05-05 17:23:22 +0800

    cor file mark5

--- R/MakeOutPut.R
+++ R/MakeOutPut.R
@@ -162,7 +162,7 @@ MakeOutPut <- function(is95=TRUE,folder_name='',openfile=TRUE){
   dirExists(rundir,verbose=TRUE)  ## This makes it
 
   # Copy correlation matrix if it exists
-  corfile_src <- file.path(getwd(), CorrelationMatrix.csv")
+  corfile_src <- file.path(getwd(), "CorrelationMatrix.csv")
   print(paste("Looking for correlation file at:", corfile_src))
   print(paste("Exists:", file.exists(corfile_src)))
   if (file.exists(corfile_src)) {
